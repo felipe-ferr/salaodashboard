338d2efa5d98ed12602aea4632c9aef398d90d72
Marcelle <marcellebsilva@outlook.com>
2024-04-01 22:06:10 -0300

    Adição servlet GerenciarServico.java

---
+++ src/java/controller/GerenciarServico.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Servico;
+import model.ServicoDAO;
+
+/**
+ *
+ * @author Hecar
+ */
+public class GerenciarServico extends HttpServlet {
+
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        PrintWriter out = response.getWriter();
+        String idServico = request.getParameter("idServico");
+        String nome = request.getParameter("nome");
+        String duracao = request.getParameter("duracao");
+        String valor = request.getParameter("valor");
+        String descricao = request.getParameter("descricao");
+        String status = request.getParameter("status");
+        
+        String mensagem="";
+        
+        Servico s = new Servico();
+        try{
+            ServicoDAO sDAO = new ServicoDAO();
+            if(!idServico.isEmpty()){
+                s.setIdServico(Integer.parseInt(idServico));
+            }
+            
+            if(nome.equals("")||nome.isEmpty()){
+                mensagem = "Campos obrigatórios deverão ser preenchidos";
+            }else{
+                s.setNome(nome);
+                if(sDAO.gravar(s)){
+                    mensagem = "Gravado com sucesso!";
+                }else{
+                    mensagem = "Erro ao gravar no banco de dados!";
+                }
+            }
+        }catch(Exception e){
+            out.print(e);
+            mensagem = "Erro ao executar";
+        }
+        
+        out.println("<script type='text/javascript'>");
+        out.println("alert('"+mensagem+"');");
+        out.println("location.href='listar_servico.jsp';");
+        out.println("</script>");
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
