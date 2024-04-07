4f3b9819972bca27b1932eb4ee32ba56d1652836
Marcelle <marcellebsilva@outlook.com>
2024-04-07 16:54:36 -0300

    Adição Agendamento

---
+++ src/java/model/Agendamento.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.sql.Time;
+import java.util.Date;
+
+public class Agendamento {
+    
+    int idagendamento;
+    float valor;
+    int status;
+    String descricao;
+    Date data_cadastro;
+    Time horario;
+    
+}
